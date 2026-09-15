c7c035b0d0e41d7ea57da0121cab0aee4cf60f6a
SonCryptoz <118987900+SonCryptoz@users.noreply.github.com>
2026-09-16 01:38:57 +0700

    fix github pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 import "./index.css";
 import App from "./App.jsx";
 
+// Dùng HashRouter vì GitHub Pages không hỗ trợ server rewrite cho các route như /sign-in, /products/1
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-            <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>,
 );
